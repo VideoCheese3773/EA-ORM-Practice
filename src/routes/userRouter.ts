36966f4c9643f236fbf93b3ad239c4d599bb59c2
VideoCheese3773 <eedduxx@gmail.com>
2023-09-29 09:00:29 -0500

    user router

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,20 +1,20 @@
 // src/routes/taskRoutes.js
 //const express = require('express');
-//const router = express.Router();
+const userRouter = express.Router();
 const UserController = require('../controller/UserController');
 
 // Obtener todas las tareas
-router.get('/users',UserController.getAllusers);
+userRouter.get('/users',UserController.getAllusers);
 // Obtener una tarea por ID
-router.get('/users/:id', UserController.getTaskById);
+userRouter.get('/users/:id', UserController.getTaskById);
 // Crear una nueva tarea
-router.post('/users', UserController.createTask);
+userRouter.post('/users', UserController.createTask);
 // Actualizar una tarea por ID
-router.put('/users/:id', UserController.updateTask);
+userRouter.put('/users/:id', UserController.updateTask);
 // Eliminar una tarea por ID
-router.delete('/users/:id', UserController.deleteTask);
+userRouter.delete('/users/:id', UserController.deleteTask);
 // Eliminar todas las tareas
-router.delete('/users/', UserController.deleteAllusers);
+userRouter.delete('/users/', UserController.deleteAllusers);
 
 //Saludar desde el API
 userRouter.get('/', UserController.sayHello);
